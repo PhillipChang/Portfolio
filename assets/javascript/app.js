5f7accfb34402f6f584bd3cbeb5fe72fd0b97326
Phillip Chang <phillip-chang@live.com>
2019-06-17 16:56:31 -0700

    Add transition between content on same page

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,26 @@
 $(document).ready(function(){
     $(".sidenav").sidenav();
     $('.carousel').carousel();
+    loadAbout();
   });
 
+  $("#about").on("click", loadAbout);
+  $("#portfolio").on("click", loadPortfolio);
+  $("#contact").on("click", loadContact);
+
+// ***Transition Pages***
+function loadAbout(){
+  $(".portfolio").delay(1000).fadeOut();
+  $(".contact").delay(1000).fadeOut();
+  $(".about").delay(1500).fadeIn();
+}  
+function loadPortfolio(){
+    $(".about").delay(1000).fadeOut();
+    $(".contact").delay(1000).fadeOut();
+    $(".portfolio").delay(1500).fadeIn();
+  }
+function loadContact(){
+    $(".about").delay(1000).fadeOut();
+    $(".portfolio").delay(1000).fadeOut();
+    $(".contact").delay(2000).fadeIn();
+  }
