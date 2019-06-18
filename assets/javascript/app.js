3a837f43e7a17cbaea36c552abcacbbeb8cbb389
Phillip Chang <phillip-chang@live.com>
2019-06-17 21:54:33 -0700

    Replace images in carousel and each image has a link to webpage

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,10 @@
 $(document).ready(function(){
     $(".sidenav").sidenav();
-    $('.carousel').carousel();
+    $('.carousel.carousel-slider').carousel({
+      fullWidth: true,
+      fullHeight:true,
+      indicators: true
+    });
     loadAbout();
   });
 
